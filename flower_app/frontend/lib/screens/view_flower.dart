f7e8ed0cdb24f56d3cfd6c8615191852821eb318
Roshni Rai <roshnirai@Roshnis-MacBook-Air.local>
2026-02-10 21:16:49 +0530

    Added flower.app project

---
+++ flower_app/frontend/lib/screens/view_flower.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../models/flower.dart';
+
+class ViewFlowerScreen extends StatefulWidget {
+  const ViewFlowerScreen({super.key});
+
+  @override
+  ViewFlowerScreenState createState() => ViewFlowerScreenState();
+}
+
+class ViewFlowerScreenState extends State<ViewFlowerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final Flower flower = ModalRoute.of(context)!.settings.arguments as Flower;
+    return Scaffold(
+      appBar: AppBar(title: Text('List Flower')),
+      body: Center(
+        child: Column(
+          children: [
+            Text("View Flower"),
+            Image.network(
+              flower.imageUrl,
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+            Text(flower.name),
+            Text(flower.description),
+            Text(flower.pdfUrl),
+          ],
+        ),
+      ),
+    );
+  }
+}
